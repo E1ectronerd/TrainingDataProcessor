941bfbd8c231e89939bc508bc92af85aa0e6dc0a
Levi Sherman <leviathan.sherman@gmail.com>
2019-02-24 14:05:16 -0600

    Adding Kdev and cmake files

---
+++ build/CMakeFiles/trainingdataprocessor.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/trainingdataprocessor.dir/main.cpp.o"
+  "CMakeFiles/trainingdataprocessor.dir/CSVdata.cpp.o"
+  "trainingdataprocessor.pdb"
+  "trainingdataprocessor"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/trainingdataprocessor.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
